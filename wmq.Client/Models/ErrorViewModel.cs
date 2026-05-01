2f72a45bb2685879e20e421f3da078b7a147fc3a
moamen <moamen8lll@gmail.com>
2026-05-02 00:41:33 +0300

    this is the repo of WalaMAQasart url shortener

---
+++ wmq.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace wmq.Client.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ wmq.Data/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace wmq.Data;
+
+public class Class1
+{
+
+}
